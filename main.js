fc65554bc4f2324dc012d6efe1f966438233f90d
cristian <crispisjam@gmail.com>
2020-07-09 19:06:04 -0500

    se agregaron carousel y footer

---
+++ main.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+        slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		},
+		
+    });
+    
+    new Glider(document.querySelector('.carousel__lista__2'), {
+		slidesToShow: 1,
+        slidesToScroll: 1,
+		dots: '.carousel__indicadores__2',
+		arrows: {
+			prev: '.carousel__anterior__2',
+			next: '.carousel__siguiente__2'
+		},
+		
+    });
+});
